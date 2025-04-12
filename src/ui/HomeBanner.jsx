6e68fd607e95703e17b1f7953f99c1f6593d2eca
alireza <malirezag@gmail.com>
2025-04-13 00:46:45 +0330

    .

--- src/ui/HomeBanner.jsx
+++ src/ui/HomeBanner.jsx
@@ -6,19 +6,16 @@ function HomeBanner() {
   return (
     <div
       className="rounded-b-xl bg-neutral-800 
-        h-screen md:justify-around flex
-     items-center pr-3 sm:pr-5 md:pr-7 lg:pr-12 xl:pr-20 flex-col-reverse sm:flex-row sm:justify-around "
+        h-screen md:justify-around flex 
+     items-center flex-col-reverse sm:flex-row sm:justify-around "
     >
       <div className="flex flex-col gap-10 md:gap-25">
-        <h1
-          className=" lg:flex-row lg:gap-2 flex flex-col text-center gap-3 sm:gap-7 sm:mt-20 mt-10
-         text-gray-200 text-4xl sm:text-4xl xl:text-5xl justify-end "
-        >
+        <h1 className=" lg:flex-row lg:gap-2 flex flex-col text-center gap-3 sm:gap-7 text-gray-200 text-4xl sm:text-4xl xl:text-5xl ">
           موسسه حقوقی <span className=" font-bold ">اندیشمندان</span>
         </h1>
         <img
           src="/logo.png"
-          className="xl:h-150 xl:w-150 w-70 h-70 sm:hidden"
+          className="xl:h-150 xl:w-150 w-80 h-85 sm:hidden"
         />
         <div className="flex flex-row justify-center lg:text-2xl ms:text-xl  mb-20 gap-2 sm:gap-2 ">
           <Modal>
