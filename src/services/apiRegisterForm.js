a21ae4fe37cf25e89445447d886229b621281c4b
alireza <malirezag@gmail.com>
2025-04-13 00:39:26 +0330

    .

--- src/services/apiRegisterForm.js
+++ src/services/apiRegisterForm.js
@@ -1,11 +1,14 @@
 import supabase from "./supabase";
 
 export async function makeRegister(newRegistration) {
+  console.log(newRegistration);
   const { data, error } = await supabase
 
     .from("register-form")
     .insert([newRegistration])
     .select();
-  if (error) throw new Error("خطای ارسال اطلاعات");
+  if (error) {
+    throw new Error(error.message);
+  }
   return { data, error };
 }
--- src/ui/HomeBanner.jsx
+++ src/ui/HomeBanner.jsx
@@ -5,21 +5,22 @@ import NavButton from "./NavButton";
 function HomeBanner() {
   return (
     <div
-      className="rounded-b-xl bg-[url(/banner.jpg)]
-       md:h-[80%] lg:h-screen bg-left bg-cover flex
-     items-center pr-3 sm:pr-5 md:pr-7 lg:pr-12 xl:pr-20  "
+      className="rounded-b-xl bg-neutral-800 
+        h-screen md:justify-around flex
+     items-center pr-3 sm:pr-5 md:pr-7 lg:pr-12 xl:pr-20 flex-col-reverse sm:flex-row sm:justify-around "
     >
-      <div
-        className="flex
-      flex-col"
-      >
+      <div className="flex flex-col gap-10 md:gap-25">
         <h1
           className=" lg:flex-row lg:gap-2 flex flex-col text-center gap-3 sm:gap-7 sm:mt-20 mt-10
-         text-gray-200 text-2xl sm:text-4xl md:text-5xl justify-end "
+         text-gray-200 text-4xl sm:text-4xl xl:text-5xl justify-end "
         >
           موسسه حقوقی <span className=" font-bold ">اندیشمندان</span>
         </h1>
-        <div className="flex flex-row  justify-center lg:text-2xl md:text-xl mt-10 sm:mt-20 mb-20  gap-2.5 ">
+        <img
+          src="/logo.png"
+          className="xl:h-150 xl:w-150 w-70 h-70 sm:hidden"
+        />
+        <div className="flex flex-row justify-center lg:text-2xl ms:text-xl  mb-20 gap-2 sm:gap-2 ">
           <Modal>
             <Modal.Open opens="1">
               <button>
@@ -43,6 +44,10 @@ function HomeBanner() {
           </Modal>
         </div>
       </div>
+      <img
+        src="/logo.png"
+        className="xl:h-150 xl:w-150 sm:h-80 sm:w-80 md:w-100 md:h-100 lg:w-120 lg:h-120 w-70 h-70 hidden sm:block"
+      />
     </div>
   );
 }
